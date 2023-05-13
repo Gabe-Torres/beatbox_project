8aec26e87ae3a77464e7c5250dd7080c8acaded3
Gabriel Torres <thomastgtorres21@gmail.com>
2023-05-13 11:26:11 -0500

    Add append method

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -1,23 +1,26 @@
-class Linked_list
-attr_reader :head
+class LinkedList
+
+    attr_reader :head
     def initialize
         @head = nil
     end
 
-    # def append(data)
-    #   if @head == nil 
-    #        @head = Node.new("doop", nil)
-    #   else 
-    #       node = @head
-            # while(node.next_node.nil?)
-            #     node = node.next_node
+    def append(data)
+      if @head == nil 
+           @head = Node.new(data)
+      end
+        data
+    #       lastnode = @head
+            # while(!lastnode.next_node.nil?)
+            #     lastnode = lastnode.next_node
             # end
-            # node.next_node = Node.new(value, nil)
+            #end of the list
+            # lastode.next_node = Node.new(value, nil)
     #   end
-    # end
+    end
 
-    # def next_node
-    #end
-    #def to_string
-    #end
+    def count
+        
+    end
+    
 end
--- spec/linked_list_spec.rb
+++ spec/linked_list_spec.rb
@@ -2,22 +2,29 @@ require "./lib/linked_list"
 require "./lib/node"
 require "pry"
 
-RSpec.describe Linked_list do
+RSpec.describe LinkedList do
     it 'exist' do
-        list = Linked_list.new
+        list = LinkedList.new
 
-        expect(list).to be_instance_of(Linked_list)
+        expect(list).to be_instance_of(LinkedList)
     end
 
     it 'has a head that is nil by default' do
-        list = Linked_list.new
+        list = LinkedList.new
 
         expect(list.head).to eq(nil)
     end
-    # it 'appends' do
-    #     list = Linked_list.new
-    #     head = nil 
 
-    #     expect(list.append).to eq("doop")
-    # end
+    it 'appends' do
+        list = LinkedList.new
+        
+        expect(list.append("doop")).to eq("doop")
+        expect(list.head.next_node).to eq(nil)
+    end
+
+    it 'counts' do
+        list = LinkedList.new
+
+        expect(list.count).to eq(1)
+    end
 end 
