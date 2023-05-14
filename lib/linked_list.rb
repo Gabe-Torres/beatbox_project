2f52789b0f2c87738239f0d94bb2c7522d40ce6e
Gabriel Torres <thomastgtorres21@gmail.com>
2023-05-13 22:34:02 -0500

    Add append method changes

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -8,13 +8,14 @@ class LinkedList
     def append(data)
         if @head == nil 
             @head = Node.new(data)
-        # else
-        #     current_node = @head
-        #     while current_node.next_node != nil
-        #         current_node = current_node.next_node
-            # end
+        else
+            current_node = @head
+            while current_node.next_node != nil
+                current_node = current_node.next_node
         end
-        data
+        current_node.next_node = Node.new(data)
+    end
+    data
     end
 
     def count 
@@ -30,6 +31,4 @@ class LinkedList
     def to_string
         @head.data
     end
-
-
 end
